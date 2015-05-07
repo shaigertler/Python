ae4e8158e59bfb1ec3116f00b06f19f27459d552
shaigertler <shaigertler@gmail.com>
2015-05-06 22:56:15 -0400

    First python program

---
+++ First go/1.py
@@ -0,0 +1,5 @@
+Python 3.5.0a4 (v3.5.0a4:413e0e0004f4, Apr 19 2015, 17:42:14) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('Hello World')
+Hello World
+>>> 
